12a00d603212bf49b7dcb07be015aaf4b359cfc9
Hung Lý <huntly1306@gmail.com>
2023-04-17 19:35:10 +0700

    hung 4.0

--- ProjectManagementPizza/ORDER.Designer.cs
+++ ProjectManagementPizza/ORDER.Designer.cs
@@ -28,19 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ORDER));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btEdit = new System.Windows.Forms.Button();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.btReturn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btAdd);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btEdit);
+            this.panel1.Controls.Add(this.btSave);
+            this.panel1.Controls.Add(this.btDelete);
+            this.panel1.Controls.Add(this.btCancel);
+            this.panel1.Controls.Add(this.btReturn);
+            this.panel1.Location = new System.Drawing.Point(894, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(288, 391);
+            this.panel1.TabIndex = 25;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Image = ((System.Drawing.Image)(resources.GetObject("btAdd.Image")));
+            this.btAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btAdd.Location = new System.Drawing.Point(27, 23);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(104, 52);
+            this.btAdd.TabIndex = 22;
+            this.btAdd.Text = "Add";
+            this.btAdd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btAdd.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(158, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 48);
+            this.button1.TabIndex = 20;
+            this.button1.Text = " Reload";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btEdit
+            // 
+            this.btEdit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btEdit.Image = ((System.Drawing.Image)(resources.GetObject("btEdit.Image")));
+            this.btEdit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btEdit.Location = new System.Drawing.Point(27, 100);
+            this.btEdit.Name = "btEdit";
+            this.btEdit.Size = new System.Drawing.Size(108, 55);
+            this.btEdit.TabIndex = 16;
+            this.btEdit.Text = "Edit  ";
+            this.btEdit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btEdit.UseVisualStyleBackColor = true;
+            // 
+            // btSave
+            // 
+            this.btSave.Image = ((System.Drawing.Image)(resources.GetObject("btSave.Image")));
+            this.btSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btSave.Location = new System.Drawing.Point(158, 100);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(97, 55);
+            this.btSave.TabIndex = 17;
+            this.btSave.Text = "Save";
+            this.btSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btSave.UseVisualStyleBackColor = true;
+            // 
+            // btDelete
+            // 
+            this.btDelete.Image = ((System.Drawing.Image)(resources.GetObject("btDelete.Image")));
+            this.btDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btDelete.Location = new System.Drawing.Point(158, 192);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(103, 47);
+            this.btDelete.TabIndex = 18;
+            this.btDelete.Text = " Delete";
+            this.btDelete.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btDelete.UseVisualStyleBackColor = true;
+            // 
+            // btCancel
+            // 
+            this.btCancel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btCancel.Image = ((System.Drawing.Image)(resources.GetObject("btCancel.Image")));
+            this.btCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btCancel.Location = new System.Drawing.Point(31, 192);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(104, 47);
+            this.btCancel.TabIndex = 21;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btCancel.UseVisualStyleBackColor = true;
+            // 
+            // btReturn
+            // 
+            this.btReturn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btReturn.Image = ((System.Drawing.Image)(resources.GetObject("btReturn.Image")));
+            this.btReturn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btReturn.Location = new System.Drawing.Point(154, 270);
+            this.btReturn.Name = "btReturn";
+            this.btReturn.Size = new System.Drawing.Size(107, 55);
+            this.btReturn.TabIndex = 19;
+            this.btReturn.Text = "Return";
+            this.btReturn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btReturn.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 409);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(876, 303);
+            this.dataGridView1.TabIndex = 26;
             // 
             // ORDER
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1075, 699);
+            this.ClientSize = new System.Drawing.Size(1191, 717);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
             this.Name = "ORDER";
             this.Text = "ORDER";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btEdit;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Button btReturn;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
