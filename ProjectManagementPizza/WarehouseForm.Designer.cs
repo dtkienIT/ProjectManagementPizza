4156ea3573bf21040883e7c83b7e67ddeb46489a
Hung Lý <huntly1306@gmail.com>
2023-05-04 09:10:05 +0700

    12

---
+++ ProjectManagementPizza/WarehouseForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace ProjectManagementPizza
+{
+    partial class WarehouseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btReturn = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btReload = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btEdit = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCID = new System.Windows.Forms.ComboBox();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.txtWname = new System.Windows.Forms.TextBox();
+            this.txtWID = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cbCID);
+            this.panel1.Controls.Add(this.txtStreet);
+            this.panel1.Controls.Add(this.txtWname);
+            this.panel1.Controls.Add(this.txtWID);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(848, 363);
+            this.panel1.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Commune ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Street";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Warehouse Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Warehouse ID";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btReturn);
+            this.panel2.Controls.Add(this.btDelete);
+            this.panel2.Controls.Add(this.btReload);
+            this.panel2.Controls.Add(this.btCancel);
+            this.panel2.Controls.Add(this.btSave);
+            this.panel2.Controls.Add(this.btEdit);
+            this.panel2.Controls.Add(this.btAdd);
+            this.panel2.Location = new System.Drawing.Point(575, 69);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(235, 270);
+            this.panel2.TabIndex = 5;
+            // 
+            // btReturn
+            // 
+            this.btReturn.Location = new System.Drawing.Point(15, 214);
+            this.btReturn.Name = "btReturn";
+            this.btReturn.Size = new System.Drawing.Size(92, 44);
+            this.btReturn.TabIndex = 6;
+            this.btReturn.Text = "Return";
+            this.btReturn.UseVisualStyleBackColor = true;
+            this.btReturn.Click += new System.EventHandler(this.btReturn_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(128, 153);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(92, 44);
+            this.btDelete.TabIndex = 5;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // btReload
+            // 
+            this.btReload.Location = new System.Drawing.Point(15, 153);
+            this.btReload.Name = "btReload";
+            this.btReload.Size = new System.Drawing.Size(92, 44);
+            this.btReload.TabIndex = 4;
+            this.btReload.Text = "Reload";
+            this.btReload.UseVisualStyleBackColor = true;
+            this.btReload.Click += new System.EventHandler(this.btReload_Click);
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(128, 90);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(92, 44);
+            this.btCancel.TabIndex = 3;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(15, 90);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(92, 44);
+            this.btSave.TabIndex = 2;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // btEdit
+            // 
+            this.btEdit.Location = new System.Drawing.Point(128, 20);
+            this.btEdit.Name = "btEdit";
+            this.btEdit.Size = new System.Drawing.Size(92, 44);
+            this.btEdit.TabIndex = 1;
+            this.btEdit.Text = "Edit";
+            this.btEdit.UseVisualStyleBackColor = true;
+            this.btEdit.Click += new System.EventHandler(this.btEdit_Click);
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(15, 20);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(92, 44);
+            this.btAdd.TabIndex = 0;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(309, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 42);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Warehouse";
+            // 
+            // cbCID
+            // 
+            this.cbCID.FormattingEnabled = true;
+            this.cbCID.Location = new System.Drawing.Point(237, 303);
+            this.cbCID.Name = "cbCID";
+            this.cbCID.Size = new System.Drawing.Size(251, 24);
+            this.cbCID.TabIndex = 3;
+            this.cbCID.SelectedIndexChanged += new System.EventHandler(this.cbCID_SelectedIndexChanged);
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(237, 244);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(304, 22);
+            this.txtStreet.TabIndex = 2;
+            // 
+            // txtWname
+            // 
+            this.txtWname.Location = new System.Drawing.Point(237, 181);
+            this.txtWname.Name = "txtWname";
+            this.txtWname.Size = new System.Drawing.Size(304, 22);
+            this.txtWname.TabIndex = 1;
+            // 
+            // txtWID
+            // 
+            this.txtWID.Location = new System.Drawing.Point(237, 111);
+            this.txtWID.Name = "txtWID";
+            this.txtWID.Size = new System.Drawing.Size(304, 22);
+            this.txtWID.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 381);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(848, 214);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // WarehouseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 602);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "WarehouseForm";
+            this.Text = "WarehouseForm";
+            this.Load += new System.EventHandler(this.WarehouseForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btReturn;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btReload;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btEdit;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCID;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.TextBox txtWname;
+        private System.Windows.Forms.TextBox txtWID;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ ProjectManagementPizza/WarehouseForm.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectManagementPizza
+{
+    public partial class WarehouseForm : Form
+    {
+        public WarehouseForm()
+        {
+            InitializeComponent();
+        }
+        string consstr = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=QuanliPizza;Integrated Security=True;Pooling=False";
+        SqlConnection conn = null;
+        DataTable accountt = null;
+        SqlDataAdapter accouter = null;
+
+        private void ResetTextAll()
+        {
+            txtWID.ResetText();
+            txtWname.ResetText();
+            txtStreet.ResetText();
+        }
+
+        void LoadData()
+        {
+            try
+            {
+                conn = new SqlConnection(consstr);
+                conn.Open();
+                accouter = new SqlDataAdapter("SELECT * FROM warehouse", conn);
+                accountt = new DataTable();
+                accouter.Fill(accountt);
+                dataGridView1.DataSource = accountt;
+
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Không lấy được dữ liệu", "Lỗi dữ liệu!");
+            }
+        }
+        void LoadDataComm()
+        {
+            try
+            {
+                conn = new SqlConnection(consstr);
+                conn.Open();
+                accouter = new SqlDataAdapter("SELECT commune_id FROM commune", conn);
+                accountt = new DataTable();
+                accouter.Fill(accountt);
+                dataGridView1.DataSource = accountt;
+                foreach (DataRow row in accountt.Rows)
+                {
+                    cbCID.Items.Add(row["commune_id"]);
+                }
+                cbCID.SelectedIndex = 0;
+
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Không lấy được dữ liệu", "Lỗi dữ liệu!");
+            }
+        }
+        private void btReturn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WarehouseForm_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            LoadDataComm();
+        }
+
+        private void btReload_Click(object sender, EventArgs e)
+        {
+            cbCID.Items.Clear();
+            LoadDataComm();
+            LoadData();
+            ResetTextAll();
+        }
+
+        private void cbCID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            LoadData();
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            { // Thực hiện lệnh
+                conn.Open();
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                // Lệnh Insert InTo
+                cmd.CommandText = "INSERT INTO warehouse (warehouse_id, warehouse_name, street, commune_id) VALUES ( @DID, @DName, @Street,@CID)";
+
+
+                cmd.Parameters.AddWithValue("@DID", txtWID.Text);
+                cmd.Parameters.AddWithValue("@DName", txtWname.Text);
+                cmd.Parameters.AddWithValue("@Street", txtStreet.Text);
+                cmd.Parameters.AddWithValue("@CID", cbCID.Text);
+
+                cmd.ExecuteNonQuery();
+                // Thông báo
+                LoadData();
+                MessageBox.Show("Đã Thêm Nhãn Hiệu thành công!!!");
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("TNhãn hiệu đã tồn tại!!!"); ResetTextAll();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btEdit_Click(object sender, EventArgs e)
+        {
+            dataGridView1_CellContentClick(null, null);
+
+            txtWID.Focus();
+        }
+
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            ResetTextAll();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int r = dataGridView1.CurrentCell.RowIndex;
+            // Chuyển thông tin từ Gridview lên các textbox ở panel
+            txtWID.Text = dataGridView1.Rows[r].Cells[0].Value.ToString();
+            txtWname.Text = dataGridView1.Rows[r].Cells[1].Value.ToString();
+            txtStreet.Text = dataGridView1.Rows[r].Cells[2].Value.ToString();
+            cbCID.Text = dataGridView1.Rows[r].Cells[3].Value.ToString();
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            LoadData();
+            if (conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+            }
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                cmd.Connection = conn;
+
+                cmd.CommandType = CommandType.Text;
+
+                cmd.CommandText = "UPDATE warehouse SET  warehouse_name= @warehouse_name, street= @street WHERE warehouse_id = @warehouse_id";
+
+                cmd.Parameters.AddWithValue("@warehouse_id", txtWID.Text);
+                cmd.Parameters.AddWithValue("@warehouse_name", txtWname.Text);
+                cmd.Parameters.AddWithValue("@street", txtStreet.Text);
+
+                cmd.ExecuteNonQuery();
+                LoadData();
+                MessageBox.Show("Đã sửa xong!");
+
+                ResetTextAll();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Lỗi khi sửa kho hàng: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+
+            DialogResult CheckYN;
+            CheckYN = MessageBox.Show("Có chắc xóa không?", "Trả lời", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (CheckYN == DialogResult.Yes)
+            {
+                // Mở kết nối
+                LoadData();
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+                conn.Open();
+                try
+                {
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+
+                    cmd.CommandType = CommandType.Text;
+                    // Lấy Row hiện tại
+                    int r = dataGridView1.CurrentCell.RowIndex;
+
+                    // Store_ID của record hiện hành
+
+                    string StoreID = dataGridView1.Rows[r].Cells[0].Value.ToString();
+
+                    // Lệnh truy vấn SQL
+
+                    cmd.CommandText = "DELETE FROM warehouse WHERE warehouse_id='" + StoreID +
+
+                    "'";
+
+                    cmd.CommandType = CommandType.Text;
+                    // Thực hiện lệnh truy vấn
+                    cmd.ExecuteNonQuery();
+
+                    // Cập nhật lại dữ liệu trên DataGridView
+
+                    LoadData();
+                    // Thông báo
+                    MessageBox.Show("Đã xóa thành công warehouse id =" + StoreID + ".");
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Không xóa được Store hiện hành.");
+
+                    MessageBox.Show("Lỗi SQL: " + ex.Number + ". " + ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
